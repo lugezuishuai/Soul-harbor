f3f4cc36c7f93a1b2d251d88ea46b2f1ccc6005c
huangdonglu <huangdonglu.jackson@bytedance.com>
2021-08-12 23:41:13 +0800

    fix: 完善了dom文件的一些方法

--- Front-end/src/utils/dom.ts
+++ Front-end/src/utils/dom.ts
@@ -17,12 +17,58 @@ export function scrollToTop(top: number, selector = '#root,body,html', needScrol
   });
 }
 
-export function getOffsetTop(dom: HTMLElement, top = 0): number {
-  const offsetTop = dom.offsetTop + top;
+export function getRelativePosition(target: HTMLElement, container: HTMLElement) {
+  const offset = { x: -1, y: -1 };
+  if (target.compareDocumentPosition(container) & Node.DOCUMENT_POSITION_CONTAINS) {
+    const offsetParent = target.offsetParent;
+    if (!offsetParent) {
+      return offset;
+    }
+    offset.x = target.offsetLeft;
+    offset.y = target.offsetTop;
 
-  if (dom.offsetParent) {
-    return getOffsetTop(dom.offsetParent as any, offsetTop);
+    if (offsetParent === container) {
+      return offset;
+    } else {
+      const result = getRelativePosition(offsetParent as HTMLElement, container);
+      offset.x += result.x;
+      offset.y += result.y;
+    }
+  }
+
+  return offset;
+}
+
+export function getOffsetTop(dom: HTMLElement, top = 0, className = ''): number {
+  const offsetTop = dom.offsetTop + top;
+  const domParent = dom.offsetParent;
+
+  if (domParent) {
+    if (!className) {
+      return getOffsetTop(domParent as any, offsetTop);
+    } else {
+      if (!domParent.className.split(' ').includes(className)) {
+        return getOffsetTop(domParent as any, offsetTop, className);
+      }
+    }
   }
 
   return offsetTop;
+}
+
+export function getOffsetLeft(dom: HTMLElement, left = 0, className = ''): number {
+  const offsetLeft = dom.offsetLeft + left;
+  const domParent = dom.offsetParent;
+
+  if (domParent) {
+    if (!className) {
+      return getOffsetLeft(domParent as any, offsetLeft);
+    } else {
+      if (!domParent.className.split(' ').includes(className)) {
+        return getOffsetLeft(domParent as any, offsetLeft, className);
+      }
+    }
+  }
+
+  return offsetLeft;
 }
