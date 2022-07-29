9e9c191dbd785f3109b6895b4324d7826e59f449
huangdonglu <huangdonglu.jackson@bytedance.com>
2022-07-29 16:16:54 +0800

    fix: 修改redux

--- Front-end/src/redux/store.ts
+++ Front-end/src/redux/store.ts
@@ -1,6 +1,6 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducers';
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
