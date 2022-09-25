e3d0bc7a1dce21dbce87992b7f24c49a3ba275f0
huangdonglu <huangdonglu.jackson@bytedance.com>
2022-09-25 20:26:11 +0800

    fix: 修改部署脚本

--- Back-end/routes/file/index.ts
+++ Back-end/routes/file/index.ts
@@ -10,8 +10,8 @@ export const router = express.Router();
 router.post('/uploadChunks', uploadChunks);
 router.get('/verifyChunks', verifyChunks);
 router.get('/mergeChunks', mergeChunks);
-router.get('/sendFile', (req, res, next) => {
-  // res.download(path.resolve(__dirname, '../../data/原图.png'));
-  res.setHeader('content-disposition', `attachment; filename="${encodeURIComponent('原图.png')}"`);
-  res.sendFile(path.resolve(__dirname, '../../data/原图.png'));
-});
+// router.get('/sendFile', (req, res, next) => {
+//   // res.download(path.resolve(__dirname, '../../data/原图.png'));
+//   res.setHeader('content-disposition', `attachment; filename="${encodeURIComponent('原图.png')}"`);
+//   res.sendFile(path.resolve(__dirname, '../../data/原图.png'));
+// });
