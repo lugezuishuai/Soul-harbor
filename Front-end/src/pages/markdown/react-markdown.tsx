7f3ba68465a0b51937d9b6f117bd6a49abf4f571
huangdonglu <huangdonglu.jackson@bytedance.com>
2022-10-01 22:06:27 +0800

    fix: reactMarkdown.css文件中publicPath变更

--- Front-end/src/pages/markdown/react-markdown.tsx
+++ Front-end/src/pages/markdown/react-markdown.tsx
@@ -5,7 +5,7 @@ import rehypeKatex from 'rehype-katex';
 import ReactMarkdown from 'react-markdown';
 import { CodeBox } from './code-box';
 import { fnv32aHashCode } from '@/utils/fnv32aHashCode';
-import 'katex/dist/katex.min.css'; // `rehype-katex` does not import the CSS for you
+import 'katex/dist/katex.min.css';
 
 interface ReactMarkdownProps {
   content: string;
